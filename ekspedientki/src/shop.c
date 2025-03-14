7942bcea5bc66c59edaa12d0186fd3f0e183f163
Arrow Święch <arrow.swiech@gmail.com>
2025-03-14 19:58:45 +0100

    death3

---
+++ ekspedientki/src/shop.c
@@ -0,0 +1,17 @@
+typedef struct customer
+{
+    struct customer* myself;
+    int* shopping_list;
+    int wallet;
+};
+
+typedef struct clerk
+{
+    
+};
+
+queue_t customer_queue;
+
+void* client_thread(void* arg){
+    
+}
